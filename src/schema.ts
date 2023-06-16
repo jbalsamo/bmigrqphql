868f19e735a5157ae952d5c140a7813ffb084168
Joseph Balsamo <joseph.balsamo@stonybrook.edu>
2023-06-16 12:04:37 -0400

    Added mutation

--- src/schema.ts
+++ src/schema.ts
@@ -6,6 +6,10 @@ const typeDefinitions = /* GraphQL */ `
     feed: [Link!]!
   }
 
+  type Mutation {
+    postLink(url: String!, description: String!): Link!
+  }
+
   type Link {
     id: ID!
     description: String!
@@ -13,33 +17,41 @@ const typeDefinitions = /* GraphQL */ `
   }
 `;
 
-// 1
 type Link = {
   id: string;
   url: string;
   description: string;
 };
 
-// 2
 const links: Link[] = [
   {
     id: "link-0",
-    url: "https://graphql-yoga.com",
-    description: "The easiest way of setting up a GraphQL server",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL",
   },
 ];
 
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
-    // 3
     feed: () => links,
   },
-  // 4
-  Link: {
-    id: (parent: Link) => parent.id,
-    description: (parent: Link) => parent.description,
-    url: (parent: Link) => parent.url,
+  Mutation: {
+    postLink: (parent: unknown, args: { description: string; url: string }) => {
+      // 1
+      let idCount = links.length;
+
+      // 2
+      const link: Link = {
+        id: `link-${idCount}`,
+        description: args.description,
+        url: args.url,
+      };
+
+      links.push(link);
+
+      return link;
+    },
   },
 };
 
